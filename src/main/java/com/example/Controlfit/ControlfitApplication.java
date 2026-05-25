65642ac78643ca2c7da9269fc7bea6ce4ceef8e7
YARA BUENO DE SOUZA <yara.bsouza2@senacsp.edu.br>
2026-05-25 10:18:06 -0300

    First commit: Estrutura inicial do ControlFit

---
+++ src/main/java/com/example/Controlfit/ControlfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.Controlfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Controlfit/ControlfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Controlfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
